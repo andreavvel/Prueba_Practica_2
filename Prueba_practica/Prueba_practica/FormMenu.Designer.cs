30a665bda1900d483b2dc0687804c843ddcc6dc4
Andrea Velásquez <67344634+andreavvel@users.noreply.github.com>
2021-10-05 14:23:04 -0600

    initial files pt.2

---
+++ Prueba_practica/Prueba_practica/FormMenu.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Prueba_practica
+{
+    partial class FormMenu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Location = new System.Drawing.Point(508, 246);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(387, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(387, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 41);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // FormMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(653, 311);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "FormMenu";
+            this.Controls.SetChildIndex(this.btnsalir, 0);
+            this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.button2, 0);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Prueba_practica/Prueba_practica/FormMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Prueba_practica
+{
+    public partial class FormMenu : Prueba_practica.FormBase
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
